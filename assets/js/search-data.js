e1a4dc083cb1b58e557d1aff1fd96a0bcbe082a4
tianyueh8erobot <71215899+tianyueh8erobot@users.noreply.github.com>
2025-12-15 07:10:52 +0000

    Deploying to gh-pages from @ tianyueh8erobot/tianyueh8erobot.github.io@d1994c656d91b9d05e91e44fb4d2ab1d746fcde4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,28 +508,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%69%61%6E%79%75%65%68%38%65%72%6F%62%6F%74@%7A%6A%75.%65%64%75.%63%6E", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=xvFUxVsAAAAJ", "_blank");
         },
       },{
         id: 'social-x',
         title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://twitter.com/", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/Modern_Gangster", "_blank");
         },
       },{
       id: 'light-theme',
